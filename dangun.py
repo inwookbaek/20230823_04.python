04440a364fa455d5f36a9d5c3458b69a312c32fb
inwookbaek <iwbaek@gmail.com>
2023-11-16 18:18:08 +0900

    20231116

--- dangun.py
+++ dangun.py
@@ -2,9 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import sys
 
 # 서울%20브롬톤
 search = input('검색단어 => ')
+# search = sys.argv[1]
 driver = webdriver.Chrome()  # 크롬웹브라우저를 생성
 driver.get(f'https://www.daangn.com/search/{search}/') # 로그인하기 위한 사이트주소
 time.sleep(1)   # 3초지연(대기시간)
