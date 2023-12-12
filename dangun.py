a9ace88a7264fcd3ee7a5f764566693cede43f13
inwookbaek <iwbaek@gmail.com>
2023-12-12 16:35:07 +0900

    20231212

--- dangun.py
+++ dangun.py
@@ -3,10 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import sys
+from bs4 import BeautifulSoup as bs
 
 # 서울%20브롬톤
-search = input('검색단어 => ')
-# search = sys.argv[1]
+# search = input('검색단어 => ')
+search = sys.argv[1]
+condition = sys.argv[2]
+
 driver = webdriver.Chrome()  # 크롬웹브라우저를 생성
 driver.get(f'https://www.daangn.com/search/{search}/') # 로그인하기 위한 사이트주소
 time.sleep(1)   # 3초지연(대기시간)
@@ -22,6 +25,17 @@ while loop:
     except:
         print(f'pages = {i}')
         break
-        
+
+soup = bs(driver.page_source, 'html.parser')
+title = soup.select('.article-title')
+region = soup.select('.article-region-name')
+price = soup.select('.article-price')
+# 
+i = 0
+while i <= len(title)-1:
+    if(condition.casefold() in title[i].text.strip().casefold()):
+        print(title[i].text.strip(), region[i].text.strip(), price[i].text.strip())    
+    i += 1
+
 print('60분후에 프로그램 종료!!, 중지하려면 ctrl+c')
 time.sleep(6000)
